7f4369c8ee1a157dcc1052da4500134085b601e6
Carlos <carlosifsm@gmail.com>
2023-10-30 23:05:49 -0300

    final

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import ButtonSubmit from './components/buttonSubm';
 import React, {useState} from 'react';
 
 function App() {
-  let [display, setDisplay]= useState(0)
-
-  let n1 = ""
-  let n2 = ""
-  let operador = ""
-  let resp = ""
-
+  let [display, setDisplay]= useState("")
+  let [n1, setN1]= useState("")
+  let [n2, setN2]= useState("")
+  let [operador, setOperador]= useState("")
+  let [resp, setResp]= useState("")
+  
   // começar lendo os numeros
   function leNumero(num){
     if(resp !== "" && operador === ""){
@@ -20,30 +19,30 @@ function App() {
     // primeiro caso: pessoa clicando o num pela primeira vez
     if(operador === ""){
       setDisplay(display+=num)
-      n1+= num //n1 = n1 + num
+      setN1(n1+=num)
     } else if(operador !== "" && n2 === ""){  // segundo caso: pessoa clicando no segundo num
       setDisplay("")
       setDisplay(display+=num)
-      n2 = num
+      setN2(num)
     } 
     else{
       setDisplay(display+=num)
-      n2+= num //n1 = n1 + num
+      setN2(n2+=num)
     }
   } 
 
   function apaga(){
-    n1 = ""
-    n2 = ""
-    operador = ""
-    resp = ""
+    setN1("")
+    setN2("")
+    setOperador("")
+    setResp("")
     setDisplay("")
   }
 
   function leOperador(op){
     if(n1 !== ""){
-      operador = op
-      setDisplay(display+=op)
+      setOperador(op);
+      setDisplay(operador);
     }
   }
 
@@ -76,41 +75,41 @@ function App() {
       }
       setDisplay(""+resp);
 
-      n1 = "" + resp;
-      n2 = "";
-      operador = "";
+      setN1("" + resp);
+      setN2("");
+      setOperador("");
     }
   }
 
 
   return (
     <div id="calculator">
-        <form action="" onsubmit="calcular()">
-            <span id="display">{display}</span>
+        <form action="" onsubmit="calcular(event)">
+          <span id="display">{display}</span>
             <section id="buttonsGrid">
                 <article id="numbersGrid">
-                    <ButtonOp text="C" fx={()=>apaga()} class="operatorButton"></ButtonOp>
+                    <ButtonOp text="C" fx={()=>apaga()}></ButtonOp>
                     <ButtonOp></ButtonOp>
-                    <ButtonOp text="^" fx={()=>leOperador("^")}></ButtonOp>
-                    <ButtonNum text="7" fx={()=>leNumero("7")}></ButtonNum>
-                    <ButtonNum text="8" fx={()=>leNumero("8")}></ButtonNum>
-                    <ButtonNum text="9" fx={()=>leNumero("9")}></ButtonNum>
-                    <ButtonNum text="4" fx={()=>leNumero("4")}></ButtonNum>
-                    <ButtonNum text="5" fx={()=>leNumero("5")}></ButtonNum>
-                    <ButtonNum text="6" fx={()=>leNumero("6")}></ButtonNum>
-                    <ButtonNum text="1" fx={()=>leNumero("1")}></ButtonNum>
-                    <ButtonNum text="2" fx={()=>leNumero("2")}></ButtonNum>
-                    <ButtonNum text="3" fx={()=>leNumero("3")}></ButtonNum>
+                    <ButtonOp text="^" fx={()=>leOperador('^')}></ButtonOp>
+                    <ButtonNum text="7" fx={()=>leNumero('7')}></ButtonNum>
+                    <ButtonNum text="8" fx={()=>leNumero('8')}></ButtonNum>
+                    <ButtonNum text="9" fx={()=>leNumero('9')}></ButtonNum>
+                    <ButtonNum text="4" fx={()=>leNumero('4')}></ButtonNum>
+                    <ButtonNum text="5" fx={()=>leNumero('5')}></ButtonNum>
+                    <ButtonNum text="6" fx={()=>leNumero('6')}></ButtonNum>
+                    <ButtonNum text="1" fx={()=>leNumero('1')}></ButtonNum>
+                    <ButtonNum text="2" fx={()=>leNumero('2')}></ButtonNum>
+                    <ButtonNum text="3" fx={()=>leNumero('3')}></ButtonNum>
                     <ButtonNum></ButtonNum>
-                    <ButtonNum text="0" fx={()=>leNumero("0")}></ButtonNum>
+                    <ButtonNum text="0" fx={()=>leNumero('0')}></ButtonNum>
                     <ButtonNum></ButtonNum>
                 </article>
                 
                 <article id="operatorsGrid">
-                    <ButtonOp text="÷" fx={()=>leOperador("/")}></ButtonOp>
-                    <ButtonOp text="x" fx={()=>leOperador("*")}></ButtonOp>
-                    <ButtonOp text="-" fx={()=>leOperador("-")}></ButtonOp>
-                    <ButtonOp text="+" fx={()=>leOperador("+")}></ButtonOp>
+                    <ButtonOp text="÷" fx={()=>leOperador('/')}></ButtonOp>
+                    <ButtonOp text="x" fx={()=>leOperador('*')}></ButtonOp>
+                    <ButtonOp text="-" fx={()=>leOperador('-')}></ButtonOp>
+                    <ButtonOp text="+" fx={()=>leOperador('+')}></ButtonOp>
                     <ButtonSubmit text="=" fx={()=>calcular()}></ButtonSubmit>
                 </article>
             </section>
